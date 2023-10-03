c11fb43afd4259f7c93e95cbe5d472c078fa0425
Finn Strehl <finn.strehl1@gmail.com>
2023-10-03 22:12:23 +0200

    translated variables
    translated some functions and variables into english

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 import csv
 
 # Variablen definieren
-jetzt = datetime.datetime.now()
+now = datetime.datetime.now()
 # Datum und Uhrzeit im folgenden Format "2023-09-26 15:47"
-formatiert = jetzt.strftime("%Y-%m-%d")
+formated = now.strftime("%Y-%m-%d")
 pfad = r'D:/Dokumente/GitHub/simpleExpenseTracker/'
 filename = "ausgaben.csv"
 csvdelimiter = ";"
@@ -27,10 +27,10 @@ else:
     print("CSV file has been created!")
 
 
-def schreibeKonto(kontoneu):
+def writebalance(newbalance):
     with open(pfad + filename, 'r') as file:
         lines = file.readlines()
-    lines[0] = f"Kontostand:;{kontoneu}\n"
+    lines[0] = f"Kontostand:;{newbalance}\n"
     with open(pfad + filename, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=csvdelimiter)
         # Hier konvertieren wir jede Zeile in eine Liste von Zeichenketten,
@@ -38,34 +38,34 @@ def schreibeKonto(kontoneu):
         writer.writerows([line.strip().split(';') for line in lines])
 
 
-def kontostand():
-    message = "Ihr Kontostand liegt nun bei: " + str(konto) + "€." + " | " + formatiert
+def currentBalance():
+    message = "Ihr Kontostand liegt nun bei: " + str(konto) + "€." + " | " + formated
     print(message)
 
 
-def neueAusgabe(betrag, zweck):
-    global formatiert
+def newExpense(betrag, zweck):
+    global formated
     global konto
     konto = konto - betrag
-    schreibeKonto(konto)
-    new_data = ["Expense", betrag, zweck, formatiert]
+    writebalance(konto)
+    new_data = ["Expense", betrag, zweck, formated]
     with open(pfad + filename, 'a', newline='') as file:
         writer = csv.writer(file, delimiter=csvdelimiter)
         writer.writerow(new_data)
-    message = "Sie haben " + str(betrag) + '€ für "' + zweck + '" ausgegeben.' + " | " + formatiert
+    message = "Sie haben " + str(betrag) + '€ für "' + zweck + '" ausgegeben.' + " | " + formated
     print(message)
 
 
-def neuesEinkommen(betrag, quelle):
-    global formatiert
+def newIncome(betrag, quelle):
+    global formated
     global konto
     konto = konto + betrag
-    schreibeKonto(konto)
-    new_data = ["Income", str(betrag), quelle, formatiert]
+    writebalance(konto)
+    new_data = ["Income", str(betrag), quelle, formated]
     with open(pfad + filename, 'a', newline='') as file:
         writer = csv.writer(file, delimiter=csvdelimiter)  # Hier wird der Delimiter gesetzt
         writer.writerow(new_data)
-    message = "Sie haben " + str(betrag) + '€ durch "' + quelle + '" erhalten.' + " | " + formatiert
+    message = "Sie haben " + str(betrag) + '€ durch "' + quelle + '" erhalten.' + " | " + formated
     print(message)
 
 
@@ -74,19 +74,19 @@ while True:
     print("1: Neue Ausgabe")
     print("2: Neues Einkommen")
     print("3: Kontostand Einsehen")
-    eingabe = int(input("\nWelche Aktion wollen Sie durchführen: "))
-    if (eingabe == 1):
+    useraction = int(input("\nWelche Aktion wollen Sie durchführen: "))
+    if (useraction == 1):
         print()
-        betrag = int(input("Betrag: "))
-        zweck = input("Zweck: ")
-        neueAusgabe(betrag, zweck)
-    elif (eingabe == 2):
+        amount = int(input("Betrag: "))
+        purpose = input("Zweck: ")
+        newExpense(amount, purpose)
+    elif (useraction == 2):
         print()
-        betrag = int(input("Betrag: "))
-        quelle = input("Quelle: ")
-        neuesEinkommen(betrag, quelle)
-    elif (eingabe == 3):
+        amount = int(input("Betrag: "))
+        source = input("Quelle: ")
+        newIncome(amount, source)
+    elif (useraction == 3):
         print()
-        kontostand()
+        currentBalance()
     else:
         print("\nBitte geben sie eine gültige Eingabe an.")
