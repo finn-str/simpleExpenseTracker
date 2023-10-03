b76d26c2daafaab2e0dfcfededde993f04d38a29
Finn Strehl <finn.strehl1@gmail.com>
2023-10-03 17:39:47 +0200

    csv instead of txt
    switched from txt files to csv file (to store data in)

--- main.py
+++ main.py
@@ -1,55 +1,73 @@
 import datetime
 import os
+import csv
 
-#Variablen definieren
+# Variablen definieren
 jetzt = datetime.datetime.now()
-formatiert = jetzt.strftime("%Y-%m-%d %H:%M") # Datum und Uhrzeit im folgenden Format "2023-09-26 15:47"
-pfad = "/workspaces/python-beginner-projects/projects/expense_tracker/"
-filename = "ausgaben.txt"
+# Datum und Uhrzeit im folgenden Format "2023-09-26 15:47"
+formatiert = jetzt.strftime("%Y-%m-%d")
+pfad = r'D:/Dokumente/GitHub/simpleExpenseTracker/'
+filename = "ausgaben.csv"
+csvdelimiter = ";"
 
-if os.path.exists("/workspaces/python-beginner-projects/projects/expense_tracker/ausgaben.txt"):
-    with open(pfad + filename, 'r') as datei:
-        erste_zeile = datei.readline()
-        print(erste_zeile.strip())  # strip() entfernt führende und abschließende Leerzeichen, einschließlich Zeilenumbrüchen
-        konto = int(erste_zeile.strip())
+if os.path.exists(pfad + filename):
+    with open(pfad + filename, 'r') as file:
+        lines = file.readlines()
+        konto = int(''.join([char for char in lines[0] if char.isdigit()])) # extract number out of String
+    print(konto)
 else:
     konto = int(input("Was ist ihr aktueller Kontostand: "))
-    print("Ihr aktueller Kontostand wurde auf " + str(konto) + " geupdatet.")
-    with open(pfad + filename, 'w') as datei:
-            datei.writelines(str(konto) + "\n")
+    data = [
+        ["Kontostand:", konto],
+        ["Type", "Amount", "Reason", "Date"],
+        ]
+    with open(pfad + filename, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=csvdelimiter)
+        writer.writerows(data)
+    print("CSV file has been created!")
+
 
 def schreibeKonto(kontoneu):
-    erstezeile = str(kontoneu)
-    # Die Datei öffnen und den Inhalt einlesen
-    with open(pfad + filename, 'r') as datei:
-        zeilen = datei.readlines()
-        # Änderungen an der ersten Zeile vornehmen
-        zeilen[0] = str(erstezeile) + "\n"
-        # Den aktualisierten Inhalt zurück in die Datei schreiben
-    with open(pfad + filename, 'w') as datei:
-            datei.writelines(zeilen)
+    with open(pfad + filename, 'r') as file:
+        lines = file.readlines()
+    lines[0] = f"Kontostand:;{kontoneu}\n"
+    with open(pfad + filename, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=csvdelimiter)
+        # Hier konvertieren wir jede Zeile in eine Liste von Zeichenketten,
+        # indem wir sie bei den Semikolons teilen, bevor wir sie schreiben.
+        writer.writerows([line.strip().split(';') for line in lines])
+
 
 def kontostand():
-    message = "Ihr Kontostand liegt nun bei " + str(konto) + "€." + " | " + formatiert
+    message = "Ihr Kontostand liegt nun bei: " + str(konto) + "€." + " | " + formatiert
     print(message)
-    return message
+
 
 def neueAusgabe(betrag, zweck):
+    global formatiert
     global konto
     konto = konto - betrag
     schreibeKonto(konto)
+    new_data = ["Expense", betrag, zweck, formatiert]
+    with open(pfad + filename, 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=csvdelimiter)
+        writer.writerow(new_data)
     message = "Sie haben " + str(betrag) + '€ für "' + zweck + '" ausgegeben.' + " | " + formatiert
     print(message)
-    return message
+
 
 def neuesEinkommen(betrag, quelle):
     global formatiert
     global konto
     konto = konto + betrag
     schreibeKonto(konto)
+    new_data = ["Income", str(betrag), quelle, formatiert]
+    with open(pfad + filename, 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=csvdelimiter)  # Hier wird der Delimiter gesetzt
+        writer.writerow(new_data)
     message = "Sie haben " + str(betrag) + '€ durch "' + quelle + '" erhalten.' + " | " + formatiert
     print(message)
-    return message
+
 
 while True:
     print("\nWählen sie von 1 bis 10:\n")
@@ -61,17 +79,14 @@ while True:
         print()
         betrag = int(input("Betrag: "))
         zweck = input("Zweck: ")
-        with open(pfad + filename, 'a') as datei:
-            datei.write(neueAusgabe(betrag, zweck) + "\n")
+        neueAusgabe(betrag, zweck)
     elif (eingabe == 2):
         print()
         betrag = int(input("Betrag: "))
         quelle = input("Quelle: ")
-        with open(pfad + filename, 'a') as datei:
-            datei.write(neuesEinkommen(betrag, quelle) + "\n")
+        neuesEinkommen(betrag, quelle)
     elif (eingabe == 3):
         print()
-        with open(pfad + filename, 'a') as datei:
-            datei.write(kontostand() + "\n")
+        kontostand()
     else:
         print("\nBitte geben sie eine gültige Eingabe an.")
